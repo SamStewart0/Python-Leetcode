24ce7c4bc5b4dde16d6dbbe2d494f2fd401e174a
Sam Stewart <samstew367@gmail.com>
2026-03-11 13:13:06 +0000

    Add groupAnagrams method to group anagrams
    Implemented a method to group anagrams from a list of strings using a dictionary.

---
+++ Group-Anagrams_49.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        sorted_words = {}
+        
+        #iterate through the strings and sort
+
+        for word in strs:
+
+            #sorted returns a list so use .join to return a string as list cannot be used as a key for dict
+
+            sorted_word = "".join(sorted(word))
+
+            #now we have our key we use setdeafault to check whether the key already exists,
+            #if the key does not exist we create it with an empty list and append word
+            #if it does we just append word
+
+            sorted_words.setdefault(sorted_word, []).append(word)
+
+            #create new list of lists using the dictionary values pairs
+
+        return(list(sorted_words.values()))
