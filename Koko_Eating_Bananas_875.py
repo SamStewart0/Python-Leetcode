a044492c77152ebc837a579a9646b6a173efbcd3
Sam Stewart <samstew367@gmail.com>
2026-04-06 19:26:02 +0100

    Create Koko_Eating_Bananas_875.py

---
+++ Koko_Eating_Bananas_875.py
@@ -0,0 +1,38 @@
+#Define L and R pointers
+
+        L = 1
+        R = max(piles)
+
+        #Define minK for case when we have a K that matches H but is not the min 
+
+        min_k = R
+
+        while L <= R:
+
+            #find midK
+
+            mid_k = (L + R) // 2
+
+            current_hours = 0
+
+            #find currentHours using midK 
+
+            for pile in piles:
+
+                current_hours += -(-pile // mid_k)
+
+            #if current_hours is less than or equal to H this is a new min
+
+            if current_hours <= h:
+
+                min_k = mid_k
+
+                R = mid_k - 1
+
+            #else Koko needs to eat more bananas
+
+            else:
+
+                L = mid_k + 1
+
+        return min_k
